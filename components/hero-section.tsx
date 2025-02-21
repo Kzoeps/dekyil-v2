e8c71cf2c50bc7ac6b4027ac6b8bff949fc27786
kzoeps <kzoepa@gmail.com>
2025-02-21 18:17:13 -0500

    feat: awdd fonts yo

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import Image, { StaticImageData } from 'next/image'
+import * as React from "react"
+import Image, { StaticImageData } from "next/image"
 
 export interface HeroSectionProps {
     image: StaticImageData | string
@@ -24,11 +24,11 @@ export default function HeroSection({
                 />
                 <div className="absolute inset-0 bg-black/50" />
             </div>
-            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-white">
-                <h1 className="mb-4 text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-white w-full max-w-6xl px-6">
+                <h1 className="mb-4 text-5xl font-playfair tracking-tight sm:text-5xl md:text-6xl whitespace-normal">
                     {title}
                 </h1>
-                <p className="text-base uppercase tracking-wider text-muted-foreground sm:text-lg md:text-xl">
+                <p className="text-sm font-worksans font-light text-white uppercase tracking-wider text-muted-foreground whitespace-normal max-w-full">
                     {description}
                 </p>
             </div>
