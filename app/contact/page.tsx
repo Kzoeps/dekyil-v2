3eb8dc98d36da8499c2638e77f21c19e73aeab51
kzoeps <kzoepa@gmail.com>
2025-02-23 21:29:03 -0500

    feat: update contact info

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,14 +5,15 @@ import Image from "next/image"
 import View from "@/public/images/view.webp"
 import { DekyilInformation } from "@/lib/constants"
 import Link from "next/link"
+import CupFlowerView from "@/public/images/cupFlowerView.webp"
 
 export default function ContactPage() {
     return (
         <main className="relative min-h-screen">
             <HeroSection
                 image={View}
-                title="Enjoy Bumthang"
-                description="Live with a scenic view of the Bumthang valley"
+                title="Get In Touch"
+                description="Chat with us"
             />
             <div className="container mx-auto px-4 py-16">
                 <h2 className="mb-12 text-center text-4xl font-playfair font-bold tracking-tight">
@@ -67,12 +68,13 @@ export default function ContactPage() {
                             <CardContent>
                                 <div className="aspect-video overflow-hidden rounded-md">
                                     <iframe
-                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.1436468724224!2d90.73772671503463!3d27.551160982850377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e19c1b7d0c9bef%3A0x7c6c0a1e15a2c3a3!2sDekyl%20Guest%20House!5e0!3m2!1sen!2sus!4v1645123456789!5m2!1sen!2sus"
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4357.405282767549!2d90.7534369!3d27.546586500000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375ee337ee4e90d1%3A0xe616b3ddd24038f4!2zRGVreWlsIEd1ZXN0IEhvdXNlIOC9luC9keC9uuC8i-C9keC9gOC-seC9suC9o-C8i-C9mOC9guC-suC9vOC9k-C8i-C9geC-seC9suC9mOC8jQ!5e1!3m2!1sen!2sus!4v1740363878917!5m2!1sen!2sus"
                                         width="100%"
                                         height="100%"
                                         style={{ border: 0 }}
                                         allowFullScreen
                                         loading="lazy"
+                                        referrerPolicy="no-referrer-when-downgrade"
                                     ></iframe>
                                 </div>
                             </CardContent>
@@ -89,19 +91,20 @@ export default function ContactPage() {
                             <CardContent>
                                 <p className="mb-4 ">
                                     Dekyl Guest House is nestled in the heart of
-                                    Bumthang, offering a perfect blend of
-                                    traditional Bhutanese hospitality and modern
-                                    comfort. Our location provides easy access
-                                    to local attractions and breathtaking views
-                                    of the surrounding landscape.
+                                    Chamkhar Town, Bumthang offering a perfect
+                                    blend of traditional Bhutanese hospitality
+                                    and modern comfort. Our location provides
+                                    easy access to local attractions and
+                                    breathtaking views of the surrounding
+                                    landscape.
                                 </p>
                                 <p className="">
-                                    We&apos;re just a short walk from the Jakar
-                                    Dzong and other cultural landmarks. Our
-                                    friendly staff can provide recommendations
-                                    for local experiences and arrange
-                                    transportation for your adventures in
-                                    Bumthang.
+                                    We&apos;re just a short 5 minute walk from
+                                    the main town and placed conveniently in the
+                                    middle of all attractions. Our friendly
+                                    staff can provide recommendations for local
+                                    experiences and arrange transportation for
+                                    your adventures in Bumthang.
                                 </p>
                             </CardContent>
                         </Card>
@@ -115,7 +118,7 @@ export default function ContactPage() {
                             <CardContent>
                                 <div className="relative aspect-video overflow-hidden rounded-md">
                                     <Image
-                                        src="/placeholder.svg?height=400&width=600"
+                                        src={CupFlowerView}
                                         alt="Scenic view from Dekyl Guest House"
                                         fill
                                         className="object-cover"
