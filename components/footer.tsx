e36b668e4590c392ef8a1ae1907c4ec91841a009
kzoeps <kzoepa@gmail.com>
2025-03-08 21:17:24 -0500

    fix: footer links and titles

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,124 +3,127 @@ import { Facebook, Instagram } from "lucide-react"
 import Link from "next/link"
 
 const MENU_LINKS = [
-    {
-        title: "Home",
-        href: "/",
-    },
-    {
-        title: "Rooms",
-        href: "/rooms",
-    },
-    {
-        title: "Contact",
-        href: "/contact",
-    },
-    {
-        title: "About Us",
-        href: "/about-us",
-    },
-    {
-        title: "Gallery",
-        href: "/gallery",
-    },
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Rooms",
+    href: "/rooms",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+  {
+    title: "About Us",
+    href: "/about-us",
+  },
+  {
+    title: "Gallery",
+    href: "/gallery",
+  },
 ]
 
 export function Footer() {
-    return (
-        <footer className="bg-gray-100 dark:bg-gray-900">
-            <div className="container mx-auto px-4 py-16">
-                <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-4">
-                    {/* About Column */}
-                    <div className="space-y-4">
-                        <h3 className="text-lg font-playfair font-semibold">
-                            About Us
-                        </h3>
-                        <p className="text-sm tracking-tight ">
-                            Experience luxury and comfort in the heart of
-                            Bumthang valley. Our guest house offers a perfect
-                            blend of traditional charm and modern amenities.
-                        </p>
-                    </div>
+  return (
+    <footer className="bg-gray-100 dark:bg-gray-900">
+      <div className="container mx-auto px-4 py-16">
+        <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-4">
+          {/* About Column */}
+          <div className="space-y-4">
+            <h3 className="text-lg font-playfair font-semibold">
+              About Us
+            </h3>
+            <p className="text-sm tracking-tight ">
+              Experience luxury and comfort in the heart of
+              Bumthang valley. Our guest house offers a perfect
+              blend of traditional charm and modern amenities.
+            </p>
+          </div>
 
-                    {/* Quick Menu Column */}
-                    <div className="space-y-4">
-                        <h3 className="text-lg font-playfair font-semibold">
-                            Quick Menu
-                        </h3>
+          {/* Quick Menu Column */}
+          <div className="space-y-4">
+            <h3 className="text-lg font-playfair font-semibold">
+              Quick Menu
+            </h3>
 
-                        <ul className="space-y-2 text-sm font-worksans tracking-tight ">
-                            {MENU_LINKS.map((link) => (
-                                <li key={link.title}>
-                                    <Link
-                                        href={link.href}
-                                        title={link.title}
-                                        className="hover:underline transition-all duration-500 ease-in-out"
-                                    >
-                                        {link.title}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+            <ul className="space-y-2 text-sm font-worksans tracking-tight ">
+              {MENU_LINKS.map((link) => (
+                <li key={link.title}>
+                  <Link
+                    href={link.href}
+                    title={link.title}
+                    className="hover:underline transition-all duration-500 ease-in-out"
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-                    {/* Contact Information Column */}
-                    <div className="space-y-4">
-                        <h3 className="text-lg font-semibold font-playfair">
-                            Contact Us
-                        </h3>
-                        <ul className="space-y-2 text-sm  font-worksans tracking-tight">
-                            <li>{DekyilInformation.DEKYIL}</li>
-                            <li>
-                                Phone: {DekyilInformation.PHONE_ACHI}/
-                                {DekyilInformation.PHONE_AMA}
-                            </li>
-                            <li>
-                                <Link
-                                    className="hover:text-red-400 transition-all duration-300 ease-in-out"
-                                    href={`mailto:${DekyilInformation.EMAIL}`}
-                                >
-                                    Email: {DekyilInformation.EMAIL}
-                                </Link>
-                            </li>
-                            <li>{DekyilInformation.ADDRESS}</li>
-                        </ul>
-                    </div>
+          {/* Contact Information Column */}
+          <div className="space-y-4">
+            <h3 className="text-lg font-semibold font-playfair">
+              Contact Us
+            </h3>
+            <ul className="space-y-2 text-sm  font-worksans tracking-tight">
+              <li>{DekyilInformation.DEKYIL}</li>
+              <li>
+                Phone: {DekyilInformation.PHONE_ACHI}/
+                {DekyilInformation.PHONE_AMA}
+              </li>
+              <li>
+                <Link
+                  title="Email Dekyil"
+                  className="hover:text-red-400 transition-all duration-300 ease-in-out"
+                  href={`mailto:${DekyilInformation.EMAIL}`}
+                >
+                  Email: {DekyilInformation.EMAIL}
+                </Link>
+              </li>
+              <li>{DekyilInformation.ADDRESS}</li>
+            </ul>
+          </div>
 
-                    {/* Social Media Column */}
-                    <div className="space-y-4">
-                        <h3 className="text-lg font-semibold font-playfair">
-                            Follow Us
-                        </h3>
-                        <div className="flex space-x-4 ">
-                            <Link
-                                href={DekyilInformation.FACEBOOK}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className=" hover:text-red-400 transition-all duration-300 ease-in-out "
-                            >
-                                <Facebook className="h-6 w-6" />
-                                <span className="sr-only">Facebook</span>
-                            </Link>
-                            <Link
-                                href={DekyilInformation.INSTAGRAM}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className=" hover:text-red-400 transition-all duration-300 ease-in-out "
-                            >
-                                <Instagram className="h-6 w-6" />
-                                <span className="sr-only">Instagram</span>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="mt-8 border-t border-gray-200 dark:border-gray-800 pt-8 text-center text-sm ">
-                    <p>
-                        &copy; {new Date().getFullYear()} Dekyil Guest House. All
-                        rights reserved.
-                    </p>
-                </div>
+          {/* Social Media Column */}
+          <div className="space-y-4">
+            <h3 className="text-lg font-semibold font-playfair">
+              Follow Us
+            </h3>
+            <div className="flex space-x-4 ">
+              <Link
+                href={DekyilInformation.FACEBOOK}
+                target="_blank"
+                title="Dekyil on Facebook"
+                rel="noopener noreferrer"
+                className=" hover:text-red-400 transition-all duration-300 ease-in-out "
+              >
+                <Facebook className="h-6 w-6" />
+                <span className="sr-only">Facebook</span>
+              </Link>
+              <Link
+                href={DekyilInformation.INSTAGRAM}
+                target="_blank"
+                title="Dekyil on Instragram"
+                rel="noopener noreferrer"
+                className=" hover:text-red-400 transition-all duration-300 ease-in-out "
+              >
+                <Instagram className="h-6 w-6" />
+                <span className="sr-only">Instagram</span>
+              </Link>
             </div>
-        </footer>
-    )
+          </div>
+        </div>
+
+        <div className="mt-8 border-t border-gray-200 dark:border-gray-800 pt-8 text-center text-sm ">
+          <p>
+            &copy; {new Date().getFullYear()} Dekyil Guest House. All
+            rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
 }
