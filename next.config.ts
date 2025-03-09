a46fac8c04a7083a7260b82e303d4c77a10a6509
kzoeps <kzoepa@gmail.com>
2025-03-08 22:06:19 -0500

    feat: update redirects

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,56 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true
+      },
+      {
+        source: '/conference.html',
+        destination: '/conference-hall',
+        permanent: true
+      },
+      {
+        source: '/suite.html',
+        destination: '/rooms/suite',
+        permanent: true
+      },
+
+      {
+        source: '/deluxe.html',
+        destination: '/rooms/deluxe',
+        permanent: true
+      },
+
+      {
+        source: '/contact.html',
+        destination: '/contact',
+        permanent: true
+      },
+
+      {
+        source: '/rooms.html',
+        destination: '/rooms',
+        permanent: true
+      },
+
+      {
+        source: '/about.html',
+        destination: '/about-us',
+        permanent: true
+      },
+      {
+        source: '/gallery.html',
+        destination: '/gallery',
+        permanent: true
+      }
+    ]
+  }
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
