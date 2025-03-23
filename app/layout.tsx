ba4c883a95c970f90c70631938504fe9ac195b6f
kzoeps <kzoepa@gmail.com>
2025-03-23 15:05:13 -0400

    feat: add google scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,54 +7,64 @@ import { DEFAULT_METADATA } from "@/lib/constants"
 import Script from "next/script"
 
 const playfair = Playfair_Display({
-  variable: "--font-playfair-display",
-  subsets: ["latin"],
+    variable: "--font-playfair-display",
+    subsets: ["latin"],
 })
 
 const workSans = Work_Sans({
-  variable: "--font-work-sans",
-  subsets: ["latin"],
+    variable: "--font-work-sans",
+    subsets: ["latin"],
 })
 
-const DESCRIPTION = "Experience warm Bhutanese hospitality at Dekyil Guest House, a family-owned, women-led hotel in Bumthang. Enjoy scenic valley views, modern amenities, and a prime location near Chamkhar town. Book your stay for a cozy and memorable retreat!"
+const DESCRIPTION =
+    "Experience warm Bhutanese hospitality at Dekyil Guest House, a family-owned, women-led hotel in Bumthang. Enjoy scenic valley views, modern amenities, and a prime location near Chamkhar town. Book your stay for a cozy and memorable retreat!"
 export const metadata: Metadata = {
-  title: {
-    template: "%s | Dekyil Guest House",
-    default: "Dekyil Guest House",
-  },
-  description: DESCRIPTION,
-  keywords:
-    "Dekyil Guest House, family-run hotel, women-led hotel, Bumthang accommodations, Chakhar valley views, Bhutanese cuisine",
-  openGraph: {
-    title: "Dekyil Guest House",
+    title: {
+        template: "%s | Dekyil Guest House",
+        default: "Dekyil Guest House",
+    },
     description: DESCRIPTION,
-    type: "website",
-    siteName: "Dekyil Guest House",
-  },
-  twitter: {
-    title: "Dekyil Guest House",
-    description: DESCRIPTION,
-    creator: "@kzoeps",
-    card: "summary_large_image",
-  },
-  ...DEFAULT_METADATA,
+    keywords:
+        "Dekyil Guest House, family-run hotel, women-led hotel, Bumthang accommodations, Chakhar valley views, Bhutanese cuisine",
+    openGraph: {
+        title: "Dekyil Guest House",
+        description: DESCRIPTION,
+        type: "website",
+        siteName: "Dekyil Guest House",
+    },
+    twitter: {
+        title: "Dekyil Guest House",
+        description: DESCRIPTION,
+        creator: "@kzoeps",
+        card: "summary_large_image",
+    },
+    ...DEFAULT_METADATA,
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${playfair.variable} ${workSans.variable} antialiased`}
-      >
-        <MainNav />
-        {children}
-        <Footer />
-        <Script strategy="lazyOnload" src="https://cloud.umami.is/script.js" data-website-id="8ae11a5f-9215-4721-94c6-4c4b86a88394" />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body
+                className={`${playfair.variable} ${workSans.variable} antialiased`}
+            >
+                <MainNav />
+                {children}
+                <Footer />
+                <Script
+                    strategy="lazyOnload"
+                    src="https://cloud.umami.is/script.js"
+                    data-website-id="8ae11a5f-9215-4721-94c6-4c4b86a88394"
+                />
+                <Script
+                    strategy="lazyOnload"
+                    src="https://www.googletagmanager.com/gtag/js?id=AW-734334045"
+                />
+                <Script strategy="lazyOnload" src="/scrips/google-tag.js" />
+            </body>
+        </html>
+    )
 }
---
+++ public/scripts/google-conversion.js
@@ -0,0 +1,1 @@
+gtag('event', 'conversion', { 'send_to': 'AW-734334045/JDE5COjm164aEN2YlN4C' });
