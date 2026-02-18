ca2303566ec9ed97070b5e10902e8d24c63d3a97
kzoeps <kzoepa@gmail.com>
2026-02-19 01:20:35 +0600

    fix: correct /scrips/ typo to /scripts/ in locale layout (dekyil-v2-jct)

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -95,7 +95,7 @@ export default async function LocaleLayout({
                     strategy="lazyOnload"
                     src="https://www.googletagmanager.com/gtag/js?id=AW-734334045"
                 />
-                <Script strategy="lazyOnload" src="/scrips/google-tag.js" />
+                <Script strategy="lazyOnload" src="/scripts/google-tag.js" />
             </body>
         </html>
     )
