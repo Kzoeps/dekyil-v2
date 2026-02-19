c100a86e37d67e94a0011a01c832ac2d8d3dd080
kzoeps <kzoepa@gmail.com>
2026-02-20 00:04:49 +0600

    Revert "Fix carousel select handler cleanup (dekyil-v2-04f.8)"
    This reverts commit 7a7fc228e3bcf878f72b28bf8463eac367d35485.

--- components/image-carousel/carousel-context.tsx
+++ components/image-carousel/carousel-context.tsx
@@ -4,70 +4,57 @@ import * as React from "react"
 import type { CarouselApi } from "@/components/ui/carousel"
 
 interface CarouselContextType {
-    api: CarouselApi | undefined
-    setApi: (api: CarouselApi | undefined) => void
-    fullscreenApi: CarouselApi | undefined
-    setFullscreenApi: (api: CarouselApi | undefined) => void
-    current: number
-    setCurrent: (index: number) => void
+  api: CarouselApi | undefined
+  setApi: (api: CarouselApi | undefined) => void
+  fullscreenApi: CarouselApi | undefined
+  setFullscreenApi: (api: CarouselApi | undefined) => void
+  current: number
+  setCurrent: (index: number) => void
 }
 
-const CarouselContext = React.createContext<CarouselContextType | undefined>(
-    undefined
-)
+const CarouselContext = React.createContext<CarouselContextType | undefined>(undefined)
 
 export function CarouselProvider({ children }: { children: React.ReactNode }) {
-    const [api, setApi] = React.useState<CarouselApi>()
-    const [fullscreenApi, setFullscreenApi] = React.useState<CarouselApi>()
-    const [current, setCurrent] = React.useState(0)
+  const [api, setApi] = React.useState<CarouselApi>()
+  const [fullscreenApi, setFullscreenApi] = React.useState<CarouselApi>()
+  const [current, setCurrent] = React.useState(0)
 
-    const handleSelect = React.useCallback(() => {
-        if (!api || !fullscreenApi) return
-        const nextIndex = api.selectedScrollSnap()
-        setCurrent(nextIndex)
-        fullscreenApi.scrollTo(nextIndex)
-    }, [api, fullscreenApi])
+  // Sync the current slide between normal and fullscreen carousels
+  React.useEffect(() => {
+    if (!api || !fullscreenApi) return
 
-    const handleFullscreenSelect = React.useCallback(() => {
-        if (!api || !fullscreenApi) return
-        const nextIndex = fullscreenApi.selectedScrollSnap()
-        setCurrent(nextIndex)
-        api.scrollTo(nextIndex)
-    }, [api, fullscreenApi])
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap())
+      fullscreenApi.scrollTo(api.selectedScrollSnap())
+    })
 
-    // Sync the current slide between normal and fullscreen carousels
-    React.useEffect(() => {
-        if (!api || !fullscreenApi) return
+    fullscreenApi.on("select", () => {
+      setCurrent(fullscreenApi.selectedScrollSnap())
+      api.scrollTo(fullscreenApi.selectedScrollSnap())
+    })
+  }, [api, fullscreenApi])
 
-        api.on("select", handleSelect)
-        fullscreenApi.on("select", handleFullscreenSelect)
-
-        return () => {
-            api.off("select", handleSelect)
-            fullscreenApi.off("select", handleFullscreenSelect)
-        }
-    }, [api, fullscreenApi, handleSelect, handleFullscreenSelect])
-
-    return (
-        <CarouselContext.Provider
-            value={{
-                api,
-                setApi,
-                fullscreenApi,
-                setFullscreenApi,
-                current,
-                setCurrent,
-            }}
-        >
-            {children}
-        </CarouselContext.Provider>
-    )
+  return (
+    <CarouselContext.Provider
+      value={{
+        api,
+        setApi,
+        fullscreenApi,
+        setFullscreenApi,
+        current,
+        setCurrent,
+      }}
+    >
+      {children}
+    </CarouselContext.Provider>
+  )
 }
 
 export function useCarousel() {
-    const context = React.useContext(CarouselContext)
-    if (!context) {
-        throw new Error("useCarousel must be used within a CarouselProvider")
-    }
-    return context
+  const context = React.useContext(CarouselContext)
+  if (!context) {
+    throw new Error("useCarousel must be used within a CarouselProvider")
+  }
+  return context
 }
+
